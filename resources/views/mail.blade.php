06bc03d5289699a40fec5d06523f00b378d1e7e5
SPremanandReddy <premanandreddysama@gmail.com>
2024-11-26 19:36:39 +0530

    laravelCrudOperationsAndEmail

---
+++ resources/views/mail.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>{{$subj}}</h1>
+<p>The content is {{$msg}}</p>
+<p>This is Sama Premanand Reddy. doing a project on sending email 
+    by laravel
+</p>
+</body>
+</html>
